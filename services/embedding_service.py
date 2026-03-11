c4a1c0e6333867df2f12a14f2c9fd1a101923d10
Mallikarjuna <spiderman05116@gmail.com>
2026-03-11 22:47:09 +0530

    Update embedding_service.py

--- services/embedding_service.py
+++ services/embedding_service.py
@@ -20,6 +20,6 @@ def get_embedding_model():
     gemini_api_key = os.getenv("GEMINI_API_KEY")
 
     return GoogleGenerativeAIEmbeddings(
-        model="text-embedding-004",
+        model="embedding-001",
         google_api_key=gemini_api_key
     )

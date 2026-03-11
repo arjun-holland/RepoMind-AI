cfe453cb8da75d0834ef784736d1a267d8491d95
Mallikarjuna <spiderman05116@gmail.com>
2026-03-11 23:28:28 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -57,7 +57,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'backend.urls'
 
